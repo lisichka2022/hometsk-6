f87e5dba360d6a6d708edc83b96e9c888b84c4c8
lisichka2022 <99811204+lisichka2022@users.noreply.github.com>
2022-12-12 22:13:14 +0200

    Add files via upload

---
+++ home6.js
@@ -0,0 +1,9 @@
+'use strict'
+
+let hour=+prompt('Input number of hours' );
+let time=60*60;
+let result=null;
+result=time*hour;
+   
+
+alert (result);
---
+++ home7.js
@@ -0,0 +1,22 @@
+let num = 1;
+
+//2
+num +=12;
+
+//3
+num -=  14;
+
+//4
+num *= 5;
+
+//5
+num /= 7;
+
+//6
+++num ;
+
+//7
+--num ;
+
+// result
+alert(num);
