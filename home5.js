b3c50df2f1035d665e191451730607688b6e0078
lisichka2022 <99811204+lisichka2022@users.noreply.github.com>
2022-12-12 22:10:24 +0200

    Add files via upload

---
+++ home5.js
@@ -0,0 +1,13 @@
+Boolean(Number(’10’)) + 1; //ВЕРНЕТ true1
+
+//2
+‘sub ’ + Number(false);  // sub0
+
+//3
+16  *  ‘      91    ‘   //1456
+
+//4
+true-70  //  -69
+
+//5
+Number(1 + String(1)) + 1  // 12
